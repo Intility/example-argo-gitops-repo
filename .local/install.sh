3332df8cff1ec3544c0d9510e8b0715e8c3c9b8a
Stian Frøystein <stianfroy@gmail.com>
2023-09-18 14:21:10 +0900

    Add some error handling

--- .local/install.sh
+++ .local/install.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
-GREEN=$(tput setaf 2)
 BLUE=$(tput setaf 4)
 NORMAL=$(tput sgr0)
 BRIGHT=$(tput bold)
 COLOR=$BLUE
-STAR='\U1F31F'
+STAR='🌟 '
 
-kind create cluster --name my-cluster --config .local/kind-config.yaml
+if ! command -v kind &> /dev/null
+then
+  echo "kind could not be found. Please install kind and try again."
+  exit 1
+fi
 
-printf "\n${STAR}${COLOR}installing nginx ingress controller...${NORMAL}\n"
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+if ! kind create cluster --name my-cluster --config .local/kind-config.yaml
+then
+  echo "Failed to create kind cluster. Exiting..."
+  exit 1
+fi
 
-printf "\n${STAR}${COLOR}waiting for ingress controller to get ready...${NORMAL}\n"
+if [[ $(kubectl config current-context) != "kind-my-cluster" ]]; then
+  echo "Current context is not kind-my-cluster. Please switch to the correct context and try again."
+  exit 1
+fi
+
+printf "\n%s%s%s\n" "$STAR" "$COLOR" "installing nginx ingress controller..."
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml || exit 1
+
+printf "\n%s%s%s\n" "$STAR" "$COLOR" "waiting for ingress controller to get ready..."
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=90s
 
-printf "\n${STAR}${COLOR}installing argocd...${NORMAL}\n"
-kubectl apply -k argo
+printf "\n%s%s%s\n" "$STAR" "$COLOR" "installing argocd..."
+kubectl apply -k .local/argo || exit 1
 
-printf "\n${STAR}${COLOR}waiting for argocd to get ready...${NORMAL}\n"
+printf "\n%s%s%s\n" "$STAR" "$COLOR" "waiting for argocd to get ready..."
 kubectl wait -n argocd \
   --for=condition=ready pod \
   --timeout=90s \
@@ -30,7 +44,9 @@ kubectl wait -n argocd \
 argo_password=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 argo_host=$(kubectl -n argocd get ingress argocd-server-ingress -o jsonpath="{.spec.rules[0].host}")
 argo_path=$(kubectl get ingress -n argocd argocd-server-ingress -o jsonpath="{.spec.rules[0].http.paths[0].path}")
-printf "\nArgoCD now available at http://${argo_host:=localhost}$argo_path"
-printf "\nusername: ${BRIGHT}admin${NORMAL}"
-printf "\npassword: ${BRIGHT}$argo_password${NORMAL}"
+printf "\nArgoCD now available at http://%s%s" "${argo_host:=localhost}" "$argo_path"
+printf "\nusername: %sadmin%s" "${BRIGHT}" "${NORMAL}"
+printf "\npassword: %s%s%s\n" "${BRIGHT}" "$argo_password" "${NORMAL}"
 
+echo ""
+echo "You can now create a bootstrap app by running 'kubectl apply -f .bootstrap/dev.yaml'"
